ff6ed2c2dc1427a57b33b416c55a88baa64b6f41
diligentliu <3391569802@qq.com>
2024-02-22 10:52:19 +0800

    V1.0

--- Makefile
+++
@@ -1,5 +0,0 @@
-server:
-	g++ util.cpp server.cpp -o server && \
- 	g++ util.cpp client.cpp -o client
-clean:
-	rm server && rm client
--- util.h
+++
@@ -1,6 +0,0 @@
-#ifndef LINUX_NET_CODING_UTIL_H
-#define LINUX_NET_CODING_UTIL_H
-
-void errif(bool , const char*);
-
-#endif //LINUX_NET_CODING_UTIL_H
